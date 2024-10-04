6b3431deaa481b87b0a3f2d73235afcbb02dd0b5
Natoria Ray <46578731+natoriaray@users.noreply.github.com>
2024-10-03 19:19:49 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+var itemQty;
+
+var cartClicked = function() {
+  alert("Cart was clicked")
+}
+
+var increase = function() {
+  document.querySelectorAll(".incDecBtn").forEach((incBtn) => incBtn.addEventListener('click', function() {
+    var qtyDisplay = this.
+    
+  }))
+}
+
